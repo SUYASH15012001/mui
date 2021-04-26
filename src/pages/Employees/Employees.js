fa3ad9f49ac55fbc5f24ecc33ea1cefacd63d72c
Suyash Pratap Singh <sakshams1501@gmail.com>
2021-04-26 17:30:02 +0530

    basic table made

--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import EmployeeForm from './EmployeeForm'
+import React, { useState } from 'react'
+// import EmployeeForm from './EmployeeForm'
 import GroupIcon from '@material-ui/icons/Group';
 import PageHeader from '../../Components/PageHeader'; 
-import { Paper, makeStyles } from '@material-ui/core';
+import { Paper, makeStyles, TableBody, TableRow, TableCell } from '@material-ui/core';
+import useTable from '../../Components/useTable';
+import * as employeeService from '../../services/employeeService';
 
 const useStyles = makeStyles(theme=> ({
     pageContent:{
@@ -11,8 +13,20 @@ const useStyles = makeStyles(theme=> ({
     }
 }))
 
+const headCells = [
+    { id: 'fullName', label: 'Employee Name' },
+    { id: 'email', label: 'Email Address (Personal)' },
+    { id: 'mobile', label: 'Mobile Number' },
+    { id: 'department', label: 'Department' },
+]
+
+
 function Employees() {
     const classes = useStyles();
+
+    const [records, setRecords] = useState(employeeService.getAllEmployees())
+
+    const {TblContainer, TblHead} = useTable(records,headCells);
     return (
         <>
             <PageHeader 
@@ -21,7 +35,22 @@ function Employees() {
                 icon={<GroupIcon fontSize='large'/>}
             />
             <Paper className={classes.pageContent}>
-                <EmployeeForm/>
+                {/* <EmployeeForm/> */}
+                <TblContainer>
+                    <TblHead/>
+                    <TableBody>
+                    {
+                        records.map(item=> (
+                            <TableRow key={item.id}>
+                                <TableCell>{item.fullName}</TableCell>
+                                <TableCell>{item.email}</TableCell>
+                                <TableCell>{item.mobile}</TableCell>
+                                <TableCell>{item.department}</TableCell>
+                            </TableRow>
+                        ))
+                    }
+                    </TableBody>
+                </TblContainer>
             </Paper>
         </>
     )
